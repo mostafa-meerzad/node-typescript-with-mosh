1d825d3979f10b2e2ddb971bd25ecb435e668993
mostafa <mostafameerzad@gmail.com>
2024-08-19 13:20:07 +0430

    create a basic express file

--- index.ts
+++ index.ts
@@ -1,2 +1,10 @@
-let msg: string = "Hello World"
-console.log(msg)
+import express from "express"
+
+const app: express.Application = express()
+
+app.get("/", (req, res)=>{
+
+    res.send("Hello")
+})
+
+app.listen(3000,() => console.log(`server listening on port: 3000`))
