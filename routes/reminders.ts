d5c1fce3b1a08b84a86691d0ac902d7a7b5770f6
mostafa <mostafameerzad@gmail.com>
2024-08-19 14:42:57 +0430

    handle creating reminders

--- routes/reminders.ts
+++ routes/reminders.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import CreateReminderDto from "../dto/createReminders";
+import Reminder from "../models/reminder";
 
 const router = Router();
 // dto => data transfer object
@@ -8,14 +9,27 @@ const router = Router();
 //   title: string;
 // }
 
+// create an array for storing reminders
+
+const reminders: Reminder[] = [];
+
 router.get("/", (req, res) => {
-  res.send("list of reminders");
+  res.json(reminders);
 });
 
 router.post("/", (req, res) => {
   //  (req.body as CreateReminderDto).title;// now only properties described in the interface are allowed
-  const { title } = req.body as CreateReminderDto; // we only need the title 
-  res.json(title);
+  const { title } = req.body as CreateReminderDto; // we only need the title
+  //   res.json(title);
+  // implement this route to store reminders
+
+  // const reminder = {id:Date.now(), title, isCompleted: false}// generating an id and setting siCompleted to true/false is not the responsibility of a router, a router must only handle routing
+
+  // after creating reminder model
+
+  const reminder = new Reminder(title); // now we only need to pass title and we will have a reminder object
+  reminders.push(reminder);
+  res.status(201).send(reminder); // send the newly created reminder
 });
 
 export default router;
